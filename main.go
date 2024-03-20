b9ec7f9e016250261a3b522487fca0307a561fa8
Alexios Kalpakidis <akalpaki@proton.me>
2024-03-20 19:01:22 +0200

    refactor crud

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"flag"
 	"fmt"
 	"os"
-	"strings"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -21,42 +21,151 @@ const BANNER = `
 A minimalistic CLI todo-app (may get extended to include web UI)
 Purpose:
 - todo get: retrieve stored todo items
-- todo add: add an item to the todo list
+- todo create: create an item to the todo list
 - todo delete: delete stored todo items
 `
 
 const MIGRATION_TEMP = `
-create table if not exists todo (id integer not null primary key autoincrement, todo text);
+create table if not exists todo (id integer not null primary key autoincrement, name text not null);
+create table if not exists todo_item (id integer not null primary key autoincrement, content text, done boolean not null, todo_id integer not null, foreign key (todo_id) references todo (id) on delete cascade);
 `
 
-func addTodo(db *sql.DB, todo string) error {
-	_, err := db.Exec("insert into todo (todo) values (?)", todo)
+type todo struct {
+	id    int
+	name  string
+	items []todoItem
+}
+
+type todoItem struct {
+	id      int
+	content []string
+	done    bool
+	todoID  int
+}
+
+// createTodo creates a new Todo list. If todo items are passed, they are added to the list.
+func createTodo(ctx context.Context, db *sql.DB, td todo) (todo, error) {
+	addItem, prepErr := db.Prepare("insert into todo_item(content, done, todo_id) values (?, ?, ?)") // TODO: look into preparing these once somewhere, maybe sync once?
+	if prepErr != nil {
+		return todo{}, prepErr
+	}
+
+	tx, err := db.BeginTx(ctx, nil)
+	if err != nil {
+		return todo{}, err
+	}
+	txAddItem := tx.Stmt(addItem)
+
+	_, err = tx.Exec("insert into todo (name) values ?", td.name)
+	if err != nil {
+		tx.Rollback()
+		return todo{}, err
+	}
+
+	if len(td.items) > 0 {
+		for i, v := range td.items {
+			res, err := txAddItem.Exec(v.content, v.done, v.todoID)
+			if err != nil {
+				tx.Rollback()
+				return todo{}, err
+			}
+			id, err := res.LastInsertId()
+			if err != nil {
+				tx.Rollback()
+				return todo{}, err
+			}
+			td.items[i].id = int(id)
+		}
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return todo{}, err
+	}
+
+	return td, nil
+}
+
+// addTodoItems adds any number of todo items for a specific todo list
+func addTodoItems(ctx context.Context, db *sql.DB, items []todoItem) ([]todoItem, error) {
+	addItem, err := db.Prepare("insert into todo_item (content, done, todo_id) values (?, ?, ?)")
+	if err != nil {
+		return nil, err
+	}
+
+	tx, err := db.BeginTx(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	txAddItem := tx.Stmt(addItem)
+	for i, v := range items {
+		res, err := txAddItem.ExecContext(ctx, v.content, v.done, v.todoID)
+		if err != nil {
+			tx.Rollback()
+			return nil, err
+		}
+		id, err := res.LastInsertId()
+		if err != nil {
+			tx.Rollback()
+			return nil, err
+		}
+		items[i].id = int(id)
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return nil, err
+	}
+
+	return items, err
+}
+
+// updateTodoItem can be used to update an individual item from a todo list
+func updateTodoItem(ctx context.Context, db *sql.DB, item todoItem) (todoItem, error) {
+	_, err := db.ExecContext(ctx, "update todo_item (content, done) where id = ?", item.id)
+	if err != nil {
+		return todoItem{}, err
+	}
+	return item, err
+}
+
+func deleteTodo(ctx context.Context, db *sql.DB, id int) error {
+	_, err := db.ExecContext(ctx, "delete from todo where id = ?", id)
 	return err
 }
 
-func getTodo(db *sql.DB) (string, error) {
-	res := ""
-	rows, err := db.Query("select todo from todo")
+func deleteTodoItem(ctx context.Context, db *sql.DB, id int) error {
+	_, err := db.ExecContext(ctx, "delete from todo_item where id = ?", id)
+	return err
+}
+
+func getTodo(ctx context.Context, db *sql.DB, id int) (todo, error) {
+	var res todo
+
+	tdRow := db.QueryRowContext(ctx, "select * from todo where id = ?", id)
+	if err := tdRow.Scan(&res); err != nil {
+		return todo{}, err
+	}
+
+	itemRows, err := db.QueryContext(ctx, "select * from todo_item where todo_id = ?", id)
 	if err != nil {
-		return "", err
+		return todo{}, err
 	}
-	for rows.Next() {
-		newStr := ""
-		if err := rows.Scan(&newStr); err != nil {
-			return "", err
+
+	for itemRows.Next() {
+		var item todoItem
+		if err := itemRows.Scan(&item); err != nil {
+			return todo{}, err
 		}
-		res += "\n"
-		res += newStr
+		res.items = append(res.items, item)
 	}
+
 	return res, nil
 }
 
-func deleteTodo(db *sql.DB) error {
-	_, err := db.Exec("drop table if exists todo")
-	return err
-}
-
 func main() {
+	ctx := context.Background()
 	db, err := sql.Open("sqlite3", "./todo.db")
 	if err != nil {
 		fmt.Println(err)
@@ -70,7 +179,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	add := flag.NewFlagSet("add", flag.ExitOnError)
+	create := flag.NewFlagSet("create", flag.ExitOnError)
 	get := flag.NewFlagSet("get", flag.ExitOnError)
 	delete := flag.NewFlagSet("delete", flag.ExitOnError)
 
@@ -80,13 +189,18 @@ func main() {
 	}
 
 	switch os.Args[1] {
-	case "add":
-		add.Parse(os.Args[2:])
-		if err := addTodo(db, strings.Join(os.Args[2:], " ")); err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		fmt.Println("Todo Added")
+	case "create":
+		// create.Parse(os.Args[2:])
+
+		// td := todo{
+		// 	name: os.Args[2],
+		// }
+
+		// if err := createTodo(ctx, db, strings.Join(os.Args[2:], " ")); err != nil {
+		// 	fmt.Println(err)
+		// 	os.Exit(1)
+		// }
+		// fmt.Println("Todo Added")
 	case "get":
 		get.Parse(os.Args[2:])
 		todos, err := getTodo(db)
