31bb67badda5b5175f54a8b0d9cad71e18a6fad1
Alexios Kalpakidis <akalpaki@proton.me>
2024-05-27 20:46:35 +0300

    new start

--- internal/user/sql.go
+++
@@ -1,7 +0,0 @@
-package user
-
-const (
-	insert       = "INSERT INTO users (id, email, password) VALUES ($1, $2, $3)"
-	queryByEmail = "SELECT id, email, password FROM users WHERE email = $1"
-	queryByID    = "SELECT id, email, password FROM users WHERE id = $1"
-)
--- pkg/db/offset.go
+++
@@ -1,9 +0,0 @@
-package db
-
-func CalculateOffset(page, pageSize int) int {
-	offset := (page - 1) * pageSize
-	if offset < 0 {
-		offset = offset * -1
-	}
-	return offset
-}
